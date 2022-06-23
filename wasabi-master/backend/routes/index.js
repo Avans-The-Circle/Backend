98b50730d429efe768d838ca65d6831a8e211d42
daviddierckx <62950437+daviddierckx@users.noreply.github.com>
2022-06-23 10:08:39 +0200

    Update index.js

--- wasabi-master/backend/routes/index.js
+++ wasabi-master/backend/routes/index.js
@@ -18,6 +18,18 @@ router.post('/api/chat', (req, res) => {
         console.log(data)
     })
 })
-
+router.post('/api/chat', (req, res) => {
+    console.log("Chat message send");
+    const chat = new Chat({
+        Stream: req.body.stream,
+        Sender: req.body.sender,
+        Message: req.body.message,
+        Timestamp: req.body.timestamp
+    });
+    chat.save((err, data) => {
+        res.status(200).json({ code: 200, message: 'Chat Added Succesfully ', addChat: data })
+        console.log(data)
+    })
+})
 module.exports = router;
 
